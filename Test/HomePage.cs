4d938684de601adb4a309a873a0cda001d5b585e
Maxence MONTET <maxence.montet@lojelis.com>
2019-09-18 09:28:55 +0200

    first and last one

---
+++ Test/HomePage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace Test
+{
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            Grid = new Grid
+            {
+                RowDefinitions = new RowDefinitionCollection() { new RowDefinition() { Height = GridLength.Star } },
+                ColumnDefinitions = new ColumnDefinitionCollection() { new ColumnDefinition() { Width = GridLength.Star } },
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                ColumnSpacing = 0,
+                RowSpacing = 0,
+            };
+        }
+
+        Grid Grid { get; set; }
+        bool IsGridSet;
+        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (propertyName == ContentProperty.PropertyName && Content != null)
+            {
+                if (!IsGridSet)
+                {
+                    //IsGridSet = true;
+                    //Grid.Children.Add(Content);
+                    //Content = Grid;
+                }
+            }
+        }
+    }
+}
